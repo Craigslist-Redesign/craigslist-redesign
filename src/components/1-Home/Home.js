5975f9f6f6fae5dd9684f42ae3c08d83606e8fa0
Aj Wietecha II <ajwietechaii@yahoo.com>
2017-11-01 11:48:27 -0500

    made a card for sale passing in tags

--- src/components/1-Home/Home.js
+++ src/components/1-Home/Home.js
@@ -1,10 +1,11 @@
 import React from 'react';
-
+import CardForSale from './Cards/CardForSale';
 
 export default function Home(){
   return(
     <div>
-      <h1>HOME</h1>  
+      <h1>HOME</h1>
+      <CardForSale />
     </div>
   )
 }
--- src/components/2-Listings/Listings.js
+++ src/components/2-Listings/Listings.js
@@ -1,10 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 
-export default function Postings(){
+class Listings extends Component {
+constructor(props){
+  super(props);
+
+this.state = {
+  tag: '',
+}
+
+}
+componentWillMount(){
+  const tag = this.props.match.params.tag;
+  console.log(tag);
+ this.setState({})
+}
+
+
+  render(){
   return(
     <div>
       <h1>Listings</h1>
     </div>
   )
+ }
 }
+
+export default Listings;
+
+// this.props.match.history
