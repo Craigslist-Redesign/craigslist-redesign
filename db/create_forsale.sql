31f29efedcbf6c8e66725e93afb6ef47dfdaafca
Russell Wright <russell.bwright@yahoo.com>
2017-11-01 09:19:21 -0500

    from changes

--- db/create_forsale.sql
+++ db/create_forsale.sql
@@ -1,4 +1,4 @@
-INSERT INTO for_sale (title, forsale_cat, price, description, location, make, model, size, condition)
-VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9);
+INSERT INTO for_sale (title, forsale_cat, price, description, location, make, model, size, condition, zipcode)
+VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10);
 SELECT * FROM for_sale
 WHERE title = ($1);
