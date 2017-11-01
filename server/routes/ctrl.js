3fdd0c76873822239c518a49c31f645deb9599ae
Riley Adair <rileydadair@gmail.com>
2017-11-01 18:57:19 -0500

    deleted get_items

--- server/routes/ctrl.js
+++ server/routes/ctrl.js
@@ -1,13 +1,5 @@
 module.exports = {
 
-  getItems: (req, res) => {
-    req.app
-    .get('db')
-    .get_items()
-    .then(items => res.json(items))
-    .catch(err => res.json(err))
-  },
-
   createUser: (req, res) => {
     console.log(req.body);
     req.app
@@ -51,6 +43,6 @@ module.exports = {
     .then(posts => res.json(posts))
     .catch(err => res.json(err))
   },
-  
+
 
 }
