b71436eaeed53ff665bdf4d27433845a8bc19d48
Aj Wietecha II <ajwietechaii@yahoo.com>
2017-11-02 17:54:03 -0500

    routes complete for each category

---
+++ src/components/1-Home/Cards/CardJobs.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import './Cards.css';
+
+class CardJobs extends Component {
+  constructor(props){
+    super(props);
+
+    this.state ={
+       tags: '',
+    }
+  }
+
+
+
+ render() {
+  return(
+
+      <div className="card-container">
+      <Link to="/listings/Jobs/all">
+        <h2 className="card-header">Jobs</h2>
+      </Link>
+         <ul>
+
+           <Link to="/listings/Jobs/Accounting & Finance">Accounting & Finance</Link>
+           <Link to="/listings/Jobs/Education">Education</Link>
+           <Link to="/listings/Jobs/Manufacturing">Manufacturing</Link>
+           <Link to="/listings/Jobs/Retiail Wholesale">Retiail Wholesale</Link>
+           <Link to="/listings/Jobs/Technical">Technical</Link>
+           <Link to="/listings/Jobs/Part-time">Part-time</Link>
+           <Link to="/listings/Jobs/Admin & Office">Admin & Office</Link>
+           <Link to="/listings/Jobs/Food & Bev">Food & Bev</Link>
+           <Link to="/listings/Jobs/Medical & Health">Medical & Health</Link>
+           <Link to="/listings/Jobs/Sales & Biz Dev">Sales & Biz Dev</Link>
+           <Link to="/listings/Jobs/Transport">Transport</Link>
+           <Link to="/listings/Jobs/Art & Media">Art & Media</Link>
+           <Link to="/listings/Jobs/General Labor">General Labor</Link>
+           <Link to="/listings/Jobs/Real Estate">Real Estate</Link>
+           <Link to="/listings/Jobs/Skilled Trade & Craft">Skilled Trade & Craft</Link>
+           <Link to="/listings/Jobs/Writing & Editng">Writing & Editng</Link>
+
+
+
+         </ul>
+      </div>
+
+   )
+ }
+}
+
+export default CardJobs;
--- src/components/1-Home/Home.js
+++ src/components/1-Home/Home.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import CardForSale from './Cards/CardForSale';
+import CardJobs from './Cards/CardJobs';
+import CardServices from './Cards/CardServices';
+import CardHousing from './Cards/CardHousing';
+import CardCommunity from './Cards/CardCommunity';
 
 export default function Home(){
   return(
     <div>
       <h1>HOME</h1>
       <CardForSale />
+      <CardJobs />
+      <CardServices />
+      <CardHousing />
+      <CardCommunity />
     </div>
   )
 }
