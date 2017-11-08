171d5c4b3a5a500f42061f730feebb99415bb482
Riley Adair <rileydadair@gmail.com>
2017-11-08 11:09:02 -0600

    listings style

--- src/components/2-Listings/Fav/Fav.js
+++ src/components/2-Listings/Fav/Fav.js
@@ -4,39 +4,44 @@ import firebase from '../../../firebase.js'
 // import './.css';
 
 class Fav extends Component{
-    constructor(props){
-        super(props);
+  constructor(props){
+    super(props);
 
-        this.state = {
-            
+    this.state = {}
+  }
+
+  componentWillMount() {
+     firebase.auth().onAuthStateChanged(user => {
+        if (user) {
+          this.setState({ user: user.email })
+
         }
-    }
+      })
+      // let fav = document.getElementById('list-item-favorite');
 
-    componentWillMount() {
-       
-       firebase.auth().onAuthStateChanged(user => {
-          if (user) {
-            
-            this.setState({ user: user.email })
-            
-          }
-        })
-    }
+      // document.getElementById('list-item-favorite').addEventListener('click', this.handleFavSelect);
+  }
 
-    favPost(favs){
-        this.props.onFav(favs)
-    }
+  componentWillUnmount() {
+    document.removeEventListener('click', this.handleFavSelect);
+  }
 
-    render(){
-       
-        return(
-            <div>
-                <a href="#" onClick={this.favPost.bind(this, this.props.item)}>
-                fav
-                </a>
-            </div>
-        )
-    }
+  handleFavSelect() {
+    document.getElementById('heart-o').classList.add('selected')
+  }
 
+  favPost(favs){
+      this.props.onFav(favs)
+  }
+
+  render(){
+    return(
+      <div id="list-item-favorite" onClick={this.favPost.bind(this, this.props.item)}>
+          <i id="heart-o" className="fa fa-heart-o" aria-hidden="true"></i>
+          <i id="heart" className="fa fa-heart" aria-hidden="true"></i>
+      </div>
+    )
+  }
 }
+
 export default Fav
--- src/components/2-Listings/Listings.js
+++ src/components/2-Listings/Listings.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 import { withRouter, Link } from 'react-router-dom';
 import LoginModal from '../Navbar/LoginModal/LoginModal';
 import Fav from './Fav/Fav'
-import'./Listings.css'
+import './Listings.css'
 
 class Listings extends Component {
   constructor(props){
@@ -101,13 +101,14 @@ class Listings extends Component {
       <div className="list-item-container" key={index}>
         <Link to={`/post/${item.post_id}`}>
         <div className="list-item-image-container" style={ backgroundStyle }>
-          { item.price != 0 && <p>${ item.price }</p> }
+          { item.price != 0 && <p className="list-item-price">${ item.price }</p> }
+          <Fav item={item} onFav={this.handleFavPost.bind(this)}/>
         </div>
         <div className="list-item-title-container">
           <h3>{ item.title }</h3>
         </div>
       </Link>
-      {/* <Fav item={item} onFav={this.handleFavPost.bind(this)}/> */}
+
     </div>
     )
   }
@@ -206,6 +207,7 @@ class Listings extends Component {
       <div id="searchbar-filter-container" className="searchbar-filter-container">
         <div id="searchbar-container" className="searchbar-container">
           <div className="searchbar-icon-container">
+            <p><i className="icon-heart-empty"></i></p>
             <svg className="searchbar-icon" viewBox="0 0 27 28" xmlns="http://www.w3.org/2000/svg"><title>Search</title><path d="M18.387 16.623C19.995 15.076 21 12.907 21 10.5 21 5.806 17.195 2 12.5 2 7.806 2 4 5.806 4 10.5S7.806 19 12.5 19c1.927 0 3.7-.65 5.125-1.73l4.4 5.153.76-.65-4.398-5.15zM12.5 18C8.364 18 5 14.636 5 10.5S8.364 3 12.5 3 20 6.364 20 10.5 16.636 18 12.5 18z" fill="currentColor" fillRule="evenodd"></path></svg>
           </div>
           <input className="input" placeholder="Search" onChange={ (event) => { this.handleSearchInput(event.target.value) }}/>
