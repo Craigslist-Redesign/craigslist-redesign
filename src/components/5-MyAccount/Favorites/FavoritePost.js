d117e09e4a66e5cfc11cd49590b5660ddc6de506
Riley Adair <rileydadair@gmail.com>
2017-11-08 21:42:28 -0600

    completed favorites functionality

--- src/components/5-MyAccount/Favorites/FavoritePost.js
+++ src/components/5-MyAccount/Favorites/FavoritePost.js
@@ -12,23 +12,30 @@ class FavoritePost extends Component {
     }
 
 
-    
+
     render() {
         const backgroundStyle = {
             backgroundImage: `url(${ this.props.fav.image_url })`
           }
         return (
 
-            // <Link to={`/post/${this.props.fav.post_id}`}>
-            <div className="list-item-container">
-                {/* <img className="list-item-image" src={this.props.fav.image_url} /> */}
-                <div className="list-item-image-container" style={ backgroundStyle }/>
-                <div className="list-item-title-container"> <h2>{this.props.fav.title} </h2></div>
-                
-                <a href="#" onClick={this.removeFav.bind(this, this.props.fav)}> x </a>
 
+          <div className="list-item-container">
+            <Link to={`/post/${this.props.fav.post_id}`}>
+              <div className="list-item-parent-image">
+                <div className="list-item-image-container" style={ backgroundStyle }></div>
+              </div>
+            </Link>
+            <div className="list-item-title-container">
+              <Link to={`/post/${this.props.fav.post_id}`}>
+                <h3>{this.props.fav.title}</h3>
+              </Link>
+              <div className="list-item-delete" onClick={this.removeFav.bind(this, this.props.fav)}>
+                x
+              </div>
             </div>
-            /* </Link> */
+          </div>
+
 
         )
 
@@ -37,4 +44,3 @@ class FavoritePost extends Component {
 }
 
 export default withRouter(FavoritePost)
-
