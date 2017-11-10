46bfbf21f9f3636726ab782a17a18cb52698714e
Riley Adair <rileydadair@gmail.com>
2017-11-10 16:18:10 -0600

    resolved search listings issue

--- db/search_listings.sql
+++
@@ -1,4 +0,0 @@
-SELECT * FROM posts
-WHERE (category = $1 AND tag = $2 AND LOWER(title) LIKE LOWER( CONCAT($3, '%') ))
-OR (category = $1 AND $2 = 'all' AND LOWER(title) LIKE LOWER( CONCAT($3, '%') ))
-ORDER BY post_id ASC;
