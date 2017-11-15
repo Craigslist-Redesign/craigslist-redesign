ad26d60a0d6654700597ce476a46dae5f8a4147d
Russell Wright <russell.bwright@yahoo.com>
2017-11-14 22:03:10 -0600

    her

--- server.js
+++ server.js
@@ -5,12 +5,18 @@ const { json } = require('body-parser');
 const cors = require('cors');
 const massive = require('massive');
 const masterRoutes = require('./server/masterRoutes');
+
+
 // const port = 4000;
 const app = express();
+
+
 // const { address, sendgridAPI } = require('./configs')
 // Database connection information
 // const connectionString = `${ address }`
 const axios = require('axios');
+
+
 // const sgMail = require("@sendgrid/mail");
 // sgMail.setApiKey(sendgridAPI);
 // connecting to our DB with massive
@@ -25,14 +31,7 @@ app.use('/', express.static(__dirname + '/public'));
 
 masterRoutes(app)
 
-// const msg = {
-//   to: 'jmay4826@gmail.com',
-//   from: 'test@example.com',
-//   subject: 'YA BAD',
-//   text: 'and easy to do anywhere, even with Node.js',
-//   html: '<strong>and easy to do anywhere, even with Node.js</strong>',
-// };
-// sgMail.send(msg);
+
 
 app.listen(process.env.PORT, function() {
   console.log('Server listening on port');
